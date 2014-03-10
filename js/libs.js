9d7645ca97d557788a81eed22b4d8af594674f59
Infinite Flux Master <braungoodson@gmail.com>
2014-03-10 19:20:04 -0400

    added data and server

--- js/libs.js
+++ js/libs.js
@@ -5,3 +5,24 @@ myApp.config(['$routeProvider',function($routeProvider){
 	});
 	$routeProvider.otherwise({redirectTo:'/home'});
 }]);
+myApp.controller('MasterController',['$scope','$http',function($scope,$http){
+	console.log("MasterController");
+	/*
+
+		$http({method: 'GET', url: '/someUrl'}).
+	    success(function(data, status, headers, config) {
+	      // this callback will be called asynchronously
+	      // when the response is available
+	    }).
+	    error(function(data, status, headers, config) {
+	      // called asynchronously if an error occurs
+	      // or server returns response with an error status.
+	    });
+
+	*/
+
+	$http({method:'GET',url:'http://localhost:30000'})
+		.success(function(d,s,h,c){
+			console.log(d);
+		})
+}]);
