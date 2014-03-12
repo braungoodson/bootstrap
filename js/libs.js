47e3b6c95201936f1e0c178fb393f8f859f49e0d
Braun Goodson <braun.goodson@rakutenloyalty.com>
2014-03-12 14:32:32 -0400

    done

--- js/libs.js
+++ js/libs.js
@@ -1,27 +1,50 @@
 var myApp = angular.module('myApp',['ngRoute']);
+
 myApp.config(['$routeProvider',function($routeProvider){
-	$routeProvider.when('/home',{
-		templateUrl: 'home'
+	$routeProvider.when('/orders',{
+		templateUrl: 'orders'
 	});
-	$routeProvider.otherwise({redirectTo:'/home'});
+	$routeProvider.otherwise({redirectTo:'/orders'});
 }]);
-myApp.controller('MasterController',['$scope','$http',function($scope,$http){
-	/*
 
-		$http({method: 'GET', url: '/someUrl'}).
-	    success(function(data, status, headers, config) {
-	      // this callback will be called asynchronously
-	      // when the response is available
-	    }).
-	    error(function(data, status, headers, config) {
-	      // called asynchronously if an error occurs
-	      // or server returns response with an error status.
-	    });
+myApp.factory('$ordersFactory',function(){
+	var orders = []
+	, names = []
+	, i
+	, j = groupon.orders.records
+	;
+	for (i in j) {
+		orders.push(new Order(j[i]));
+		if (!names[Name(j[i])]) {
+			names[Name(j[i])] = 1;
+			console.log(Name(j[i]));
+		} else {
+			names[Name(j[i])]++;
+		}
+	}
+	return {
+		getOrders: function () {
+			return orders;
+		}
+	};
+});
 
-	*/
-
-	$http({method:'GET',url:'http://localhost:30000'})
-		.success(function(d,s,h,c){
-			
-		});
+myApp.controller('OrdersController',['$scope','$ordersFactory',function($scope,$ordersFactory){
+	$scope.orders = $ordersFactory.getOrders();
 }]);
+
+function Order (o) {
+	var i = o.group[0].informations;
+	var m = o.measures;
+	this.oid = i.BillingId;
+	this.sid = i.Sid || 'unknown';
+	this.name = i.DealListName;
+	this.credit = m.LedgerCredit;
+	this.debit = m.LedgerDebit;
+	this.sale = m.LedgerAmount;
+	this.status = i.Status;
+}
+
+function Name (n) {
+	return n.group[0].informations.DealListName;
+}
--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var url = require('url');
-var courses = require('./courses.js');
-http.createServer(function(q,r){
-	r.writeHead(200,{
-		'Access-Control-Allow-Origin':'*',
-		'Content-Type':'application/json'});
-	r.end(JSON.stringify(courses));
-}).listen(process.env.PORT || 30000);
